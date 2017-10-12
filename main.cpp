74a096598ade93c9b95a388ad8f5137e3c86b26e
Sarita Finn <sarita.finn@gmail.com>
2017-10-12 08:19:20 -0400

    Updated main.cpp to implement a high power revolver

--- main.cpp
+++ main.cpp
@@ -43,6 +43,10 @@ int main(int argc, char** argv) {
     simulateWeapon(weapon, armor);
     delete(weapon);
 
+    weapon = WeaponFactory::getInstance()->getWeapon("revolver");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
     return 0;
 }
 
